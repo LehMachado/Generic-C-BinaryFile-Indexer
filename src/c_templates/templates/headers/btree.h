ffca1544bf8ece76f7c5f75efca5e50f0e15d06b
LehMachado <102212183+LehMachado@users.noreply.github.com>
2022-08-18 15:48:49 -0300

    Create btree.h

---
+++ src/c_templates/templates/headers/btree.h
@@ -0,0 +1,26 @@
+// CPP program to implement B* tree
+#include <bits/stdc++.h>
+using namespace std;
+  
+// This can be changed to any value - 
+// it is the order of the B* Tree
+#define N 4 
+  
+struct node {
+  
+    // key of N-1 nodes
+    int key[N - 1];
+      
+    // Child array of 'N' length
+    struct node* child[N];
+      
+    // To state whether a leaf or not; if node 
+    // is a leaf, isleaf=1 else isleaf=0
+    int isleaf;
+      
+    // Counts the number of filled keys in a node
+    int n;
+      
+    // Keeps track of the parent node
+    struct node* parent;
+};
